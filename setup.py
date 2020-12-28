16d395d63be2073ddd371aee043f21734745f163
BlackTiger145 <62836633+BlackTiger145@users.noreply.github.com>
2020-12-28 18:14:19 +0200

    update setup.py

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ setup(
     author="Itai Dotan",
     packages=find_packages(),
     description="Sudoku game with random boards and user entered board",
-    include_package_data=False,
+    include_package_data=True,
     zip_safe=False,
     install_requires=[
         'pygame'
     ],
-    extras_require={"test": "pytest"},
+    extras_require={"test": "pytest", 'exe': 'pyinstaller'},
 )
